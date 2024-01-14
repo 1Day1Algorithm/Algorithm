aff7573b65352043dc7785321f302988115668a1
Lydia <78534650+KangHayeonn@users.noreply.github.com>
2024-01-14 21:02:48 +0900

    code : Lydia_20240112 (fail)
    주사위 고르기 (프로그래머스 Lv.3) - 시간초과
    
    - A의 경우의 수 * B의 경우의 수를 하는 부분에서 arraylist를 사용하여 n*n의 최악의 경우의 수가 나와 시간 초과함
    - HashMap을 이용해 n*n의 경우의 수를 압축해서 줄일 예정

---
+++ 2week/Lydia/20240112/programmers_주사윈고르기.java
@@ -0,0 +1,108 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> aList = new ArrayList<>();
+    static ArrayList<Integer> bList = new ArrayList<>();
+    static ArrayList<Type> aWinList;
+    public class Type {
+        int win_A; // A가 승리한 확률
+        boolean[] isVisited;
+        public Type(int win_A, boolean[] isVisited) {
+            this.win_A = win_A;
+            this.isVisited = isVisited;
+        }
+    }
+    public int[] solution(int[][] dice) {
+        boolean[] isVisited = new boolean[dice.length];
+        aWinList = new ArrayList<>();
+        
+        combination(dice, isVisited, 0, dice.length/2);
+        
+        Collections.sort(aWinList, (a, b) -> b.win_A - a.win_A);
+        
+        int[] answer = new int[dice.length/2];
+        boolean[] aMaxWinList = aWinList.get(0).isVisited;
+        int idx = 0;
+        for(int i=0; i<aMaxWinList.length; i++) {
+            if(aMaxWinList[i]) {
+                answer[idx] = i+1;
+                idx += 1;
+            }
+        }
+        
+        return answer;
+    }
+    public void combination(int[][] dice, boolean[] isVisited, int idx, int target) {
+        if(target == 0) {
+            aList = new ArrayList<>();
+            bList = new ArrayList<>();
+            
+            calculate_A(dice, isVisited, 0, isVisited.length/2, 0); // A의 경우의수
+            calculate_B(dice, isVisited, 0, isVisited.length/2, 0); // B의 경우의수
+            calculate_AB(isVisited); // A와 B의 경우의수를 가지고 A가 승리할 확률을 저장
+            
+            return;
+        }
+        
+        for(int i=idx; i<isVisited.length; i++) {
+            if(!isVisited[i]) {
+                isVisited[i] = true;
+                combination(dice, isVisited, i+1, target-1);
+                isVisited[i] = false; 
+            }
+        }
+    }
+    public void calculate_A(int[][] dice, boolean[] isVisited, int idx, int target, int sum) {
+        if(target == 0) {
+            aList.add(sum);
+            return;
+        }        
+        
+        if(isVisited[idx]) {
+            for(int i=0; i<6; i++) {
+                calculate_A(dice, isVisited, idx+1, target-1, sum+dice[idx][i]);
+            }
+        } else {
+            calculate_A(dice, isVisited, idx+1, target, sum);
+        }
+    }
+    public void calculate_B(int[][] dice, boolean[] isVisited, int idx, int target, int sum) {
+        if(target == 0) {
+            bList.add(sum);
+            return;
+        }        
+        
+        if(!isVisited[idx]) {
+            for(int i=0; i<6; i++) {
+                calculate_B(dice, isVisited, idx+1, target-1, sum+dice[idx][i]);
+            }
+        } else {
+            calculate_B(dice, isVisited, idx+1, target, sum);
+        }
+    }
+    public void calculate_AB(boolean[] isVisited) {
+        Collections.sort(aList, (a, b) -> b - a); // A의 경우의 수 내림차순
+        Collections.sort(bList, (a, b) -> a - b); // B의 경우의 수 오름차순
+        
+        int A, B;
+        int win_A = 0;
+        for(int i=0; i<aList.size(); i++) {
+            for(int j=0; j<bList.size(); j++) {
+                A = aList.get(i);
+                B = bList.get(j);
+                
+                if(A > B) win_A += 1;
+                else break;
+            }
+        }
+        
+        aWinList.add(new Type(win_A, copyArray(isVisited)));
+    }
+    public boolean[] copyArray(boolean[] isVisited) {
+        boolean[] tempArr = new boolean[isVisited.length];
+        for(int i=0; i<isVisited.length; i++) {
+            tempArr[i] = isVisited[i];
+        }
+        return tempArr;
+    }
+}
